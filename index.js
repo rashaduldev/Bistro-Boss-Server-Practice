aad3f974cf19c99c442d505628503cc8726bc233
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-11-22 12:53:32 +0600

    gmail varification done

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ioitfil.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,11 +27,12 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const menucollection = client.db("userstDB").collection("users");
+    const usercollection = client.db("userstDB").collection("users");
+    const menucollection = client.db("userstDB").collection("menus");
     const reviewcollection = client.db("userstDB").collection("reviews");
     const cartcollection = client.db("userstDB").collection("carts");
 
-    app.get('/menu',async(req,res) => {
+    app.get('/menus',async(req,res) => {
         const result=await menucollection.find().toArray();
         res.send(result)
     })
@@ -40,6 +41,31 @@ async function run() {
         const result=await reviewcollection.find().toArray();
         res.send(result)
     })
+    // Users related api
+    app.post('/users',async(req,res) => {
+        const user=req.body;
+        const query = {email:user.email}
+        const existingUser=await usercollection.findOne(query);
+        if (existingUser) {
+          return res.send({ message: 'user already exists', insertedId: null })
+        }
+        const result=await usercollection.insertOne(user);
+        res.send(result)
+    })
+    // app.get('/users',async(req,res) => {
+    //     const result=await usercollection.find().toArray();
+    //     res.send(result)
+    // })
+    // app.delete('/users/:id',async(req,res) => {
+    //     const id=req.params.id;
+    //     const query={_id: new ObjectId(id)}
+    //     const result=await usercollection.deleteOne(query);
+    //     res.send(result)
+    // })
+    // ----------------------------------------------------------------
+    // post menu data read
+
+
     // ----------------------------------------------------------------
     // post cart data read
     app.get('/carts',async(req,res) => {
@@ -54,6 +80,28 @@ async function run() {
         const result=await cartcollection.insertOne(cartItem);
         res.send(result)
     })
+    // delete cart item
+    app.delete('/carts/:id',async(req,res) => {
+        const id=req.params.id;
+        const query={_id: new ObjectId(id)}
+        const result=await cartcollection.deleteOne(query);
+        res.send(result)
+    })
+    // ----------------------------------------------------------------
+    // post review data read
+    app.get('/reviews',async(req,res) => {
+        const email=req.query.email;
+        const query={email: email}
+        const result=await reviewcollection.find(query).toArray();
+        res.send(result)
+    })
+    // post review data
+    app.post('/reviews',async(req,res) => {
+        const reviewItem=req.body;
+        const result=await reviewcollection.insertOne(reviewItem);
+        res.send(result)
+    })
+    // delete review item
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
